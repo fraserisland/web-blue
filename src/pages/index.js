001d32dc044e30c4ed32cb5f50eaac01ee9ad77f
Fraser Solomon <fmsolomon1@gmail.com>
2019-07-08 19:16:50 +1000

    window check

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,19 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  console.log(window.navigator.bluetooth)
-
+  const windowBluetoothExists = typeof window !== 'undefined' && window.navigator.bluetooth
+  
   return (
     <Layout>
       <SEO title="Home" />
       <h1>navigator bluetooth: </h1>
-      {JSON.stringify(window.navigator.bluetooth)}
+      {
+        windowBluetoothExists &&
+        JSON.stringify(window.navigator.bluetooth)
+      }
     </Layout>
   )
 }
